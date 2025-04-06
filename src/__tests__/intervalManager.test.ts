bc31fed12a6acee6e8e5a83d2b24a8bb001146e9
David <dmiranda1482@gmail.com>
2025-04-06 21:18:25 +0100

    add more tests

--- src/__tests__/intervalManager.test.ts
+++ src/__tests__/intervalManager.test.ts
@@ -1,3 +1,4 @@
+import { Interval } from "..";
 import { IntervalManager } from "../intervalManager";
 
 
@@ -10,6 +11,38 @@ test("should return [[1,7]]", () => {
     manager.addInterval([1, 3]);
     manager.addInterval([5, 7]);
     manager.addInterval([2, 6]);
+    const obtainedArray = manager.getIntervals();
+    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+  
+    // Assert
+    expect(obtainedArray).toStrictEqual(expectedArray);
+  });
+
+  test("should return [[0,20]]", () => {
+    // Arrange
+    const expectedArray = [[0, 20]];
+    const manager = new IntervalManager()
+    
+    // Act
+    const intervals: Interval[] = [[0,10], [10,20]]
+    intervals.forEach((x) => manager.addInterval(x))
+
+    const obtainedArray = manager.getIntervals();
+    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+  
+    // Assert
+    expect(obtainedArray).toStrictEqual(expectedArray);
+  });
+
+  test("should return [[0,50]]", () => {
+    // Arrange
+    const expectedArray = [[0, 50]];
+    const manager = new IntervalManager()
+    
+    // Act
+    const intervals: Interval[] = [0, 3, 1, 2, 4].map((x) => [ x*10, (x+1)*10])
+    intervals.forEach((x) => manager.addInterval(x))
+
     const obtainedArray = manager.getIntervals();
     console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
   
