b642a4a07178e2903889c8fea221fd0456507f14
David <dmiranda1482@gmail.com>
2025-04-06 15:07:33 +0100

    clean up tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,4 @@
-import { add, Interval, mergeTwoIntervals } from "..";
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3);
-});
+import { Interval, mergeTwoIntervals } from "..";
 
 test("merge [0,1] with [2,3] to equal [[0,1],[2,3]]", () => {
   // Arrange
@@ -13,7 +9,7 @@ test("merge [0,1] with [2,3] to equal [[0,1],[2,3]]", () => {
   const obtainedArray = mergeTwoIntervals(interval1, interval2);
   console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
 
-  // assert
+  // Assert
   const expectedArray = [
     [0, 1],
     [2, 3],
@@ -22,33 +18,29 @@ test("merge [0,1] with [2,3] to equal [[0,1],[2,3]]", () => {
 });
 
 test("merge [0,2] with [1,3] to equal [[0,3]]", () => {
-    // Arrange
-    const interval1: Interval = [0, 2];
-    const interval2: Interval = [1, 3];
-  
-    // Act
-    const obtainedArray = mergeTwoIntervals(interval1, interval2);
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // assert
-    const expectedArray = [
-      [0, 3]
-    ];
-    expect(obtainedArray).toStrictEqual(expectedArray);
+  // Arrange
+  const interval1: Interval = [0, 2];
+  const interval2: Interval = [1, 3];
+
+  // Act
+  const obtainedArray = mergeTwoIntervals(interval1, interval2);
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+
+  // Assert
+  const expectedArray = [[0, 3]];
+  expect(obtainedArray).toStrictEqual(expectedArray);
 });
 
 test("merge [1,2] with [0,10] to equal [[0,10]]", () => {
-    // Arrange
-    const interval1: Interval = [0, 2];
-    const interval2: Interval = [0, 10];
-  
-    // Act
-    const obtainedArray = mergeTwoIntervals(interval1, interval2);
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // assert
-    const expectedArray = [
-      [0, 10]
-    ];
-    expect(obtainedArray).toStrictEqual(expectedArray);
+  // Arrange
+  const interval1: Interval = [0, 2];
+  const interval2: Interval = [0, 10];
+
+  // Act
+  const obtainedArray = mergeTwoIntervals(interval1, interval2);
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+
+  // Assert
+  const expectedArray = [[0, 10]];
+  expect(obtainedArray).toStrictEqual(expectedArray);
 });
