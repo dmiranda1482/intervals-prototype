488d7b71083db2580edeeabd173d8b90a95b3d79
David <dmiranda1482@gmail.com>
2025-04-06 16:27:53 +0100

    work in progress

--- src/index.ts
+++ src/index.ts
@@ -23,26 +23,27 @@ export function mergeTwoIntervals(interval1: Interval, interval2: Interval): Int
     return resultArray
 }
 
-function tryInternalMerges(intrevalArray: Interval[], newIntrevalArray: Interval[]): boolean {
-    newIntrevalArray = []
+function tryInternalMerges(intrevalArray: Interval[]): boolean {
+    let newIntrevalArray: Interval[] = []
     let overlapingInThisIteration = false
     for(let i=0; i< intrevalArray.length-1; i++) {
         for(let j=i+1; i< intrevalArray.length; j++) {
             const mergeArray = mergeTwoIntervals(intrevalArray[i], intrevalArray[j])
             overlapingInThisIteration = overlapingInThisIteration || (mergeArray.length < 2)
-            newIntrevalArray
+            newIntrevalArray.concat(mergeArray)
         }
     }
-    return false
+    intrevalArray = newIntrevalArray
+    return overlapingInThisIteration
 }
 
 export function mergeIntrevalIntoArray(intrevalArray: Interval[], newInterval: Interval): Interval[] {
     const maxIteration = intrevalArray.length
     let overlapingInThisIteration = false
     for(let iteration=0; iteration <= maxIteration; iteration++) {
-        overlapingInThisIteration = false
 
-
+        overlapingInThisIteration = tryInternalMerges(intrevalArray)
+        
         if (!overlapingInThisIteration) {
             break; // exits the loop
         }
