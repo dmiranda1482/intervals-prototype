10b0591940d18cb739bd9eefd332ee921f346ef8
David <dmiranda1482@gmail.com>
2025-04-06 14:57:54 +0100

    create basic test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,20 @@
-import { add } from "..";
+import { add, Interval, mergeTwoIntervals } from "..";
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
+  })
+
+
+  test('merge [0,1] with [2,3] to equal [[0,1],[2,3]]', () => {
+    // Arrange
+    const interval1: Interval = [0, 1]
+    const interval2: Interval = [2, 3]
+
+    // Act
+    const obtainedArray = mergeTwoIntervals(interval1, interval2)
+    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`)
+
+    // assert
+    const expectedArray = [[0,1],[2,3]]
+    expect(obtainedArray).toStrictEqual(expectedArray);
   })
