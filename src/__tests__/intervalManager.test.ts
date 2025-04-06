419ae534e83c821242a2365b69df671aa504ee0f
David <dmiranda1482@gmail.com>
2025-04-06 21:29:20 +0100

    add test

--- src/__tests__/intervalManager.test.ts
+++ src/__tests__/intervalManager.test.ts
@@ -49,3 +49,19 @@ test("should return [[1,7]]", () => {
     // Assert
     expect(obtainedArray).toStrictEqual(expectedArray);
   });
+
+  test("should return [[0,8], [10,11]]", () => {
+    // Arrange
+    const manager = new IntervalManager()
+    const expectedArray = [[0,8], [10,11]];
+    
+    // Act
+    const intervals: Interval[] = [[0,1],[3,4], [7,8], [10,11], [0,7]]
+    intervals.forEach((x) => manager.addInterval(x))
+
+    const obtainedArray = manager.getIntervals();
+    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+  
+    // Assert
+    expect(obtainedArray).toStrictEqual(expectedArray);
+  });
