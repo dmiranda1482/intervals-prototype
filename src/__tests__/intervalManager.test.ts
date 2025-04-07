ee4b6dc3fcaed8c7b3ddebc3fb6a5075ca3ea575
David <dmiranda1482@gmail.com>
2025-04-07 13:47:44 +0100

    format files -- no code changes

--- src/__tests__/intervalManager.test.ts
+++ src/__tests__/intervalManager.test.ts
@@ -1,177 +1,202 @@
 import { IntervalManager, Interval } from "../intervalManager";
 
-
 test("should return [[1,7]]", () => {
-    // Arrange
-    const expectedArray = [[1, 7]];
-    const manager = new IntervalManager()
-    
-    // Act
-    manager.addInterval([1, 3]);
-    manager.addInterval([5, 7]);
-    manager.addInterval([2, 6]);
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  // Arrange
+  const expectedArray = [[1, 7]];
+  const manager = new IntervalManager();
 
-  test("should return [[0,20]]", () => {
-    // Arrange
-    const expectedArray = [[0, 20]];
-    const manager = new IntervalManager()
-    
-    // Act
-    const intervals: Interval[] = [[0,10], [10,20]]
-    intervals.forEach((x) => manager.addInterval(x))
+  // Act
+  manager.addInterval([1, 3]);
+  manager.addInterval([5, 7]);
+  manager.addInterval([2, 6]);
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
 
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
 
-  test("should return [[0,50]]", () => {
-    // Arrange
-    const expectedArray = [[0, 50]];
-    const manager = new IntervalManager()
-    
-    // Act
-    const intervals: Interval[] = [0, 3, 1, 2, 4].map((x) => [ x*10, (x+1)*10])
-    intervals.forEach((x) => manager.addInterval(x))
+test("should return [[0,20]]", () => {
+  // Arrange
+  const expectedArray = [[0, 20]];
+  const manager = new IntervalManager();
 
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  // Act
+  const intervals: Interval[] = [
+    [0, 10],
+    [10, 20],
+  ];
+  intervals.forEach((x) => manager.addInterval(x));
 
-  test("should return [[0,8], [10,11]]", () => {
-    // Arrange
-    const manager = new IntervalManager()
-    const expectedArray = [[0,8], [10,11]];
-    
-    // Act
-    const intervals: Interval[] = [[0,1],[3,4], [7,8], [10,11], [0,7]]
-    intervals.forEach((x) => manager.addInterval(x))
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
 
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
 
+test("should return [[0,50]]", () => {
+  // Arrange
+  const expectedArray = [[0, 50]];
+  const manager = new IntervalManager();
 
-  test("should return [[0,50]] adds the intervals two times", () => {
-    // Arrange
-    const expectedArray = [[0, 50]];
-    const manager = new IntervalManager()
-    
-    // Act
-    const intervals: Interval[] = [0, 3, 1, 2, 4].map((x) => [ x*10, (x+1)*10])
-    intervals.forEach((x) => manager.addInterval(x))
-    intervals.forEach((x) => manager.addInterval(x))
+  // Act
+  const intervals: Interval[] = [0, 3, 1, 2, 4].map((x) => [
+    x * 10,
+    (x + 1) * 10,
+  ]);
+  intervals.forEach((x) => manager.addInterval(x));
 
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
 
-  test("should return an array with the correct size (VeryLargeNumber)", () => {
-    // Arrange
-    const VeryLargeNumber = 500
-    const manager = new IntervalManager()
-    
-    // Act
-    for(let i=1; i<=VeryLargeNumber; i++) {
-        manager.addInterval([i, i+0.5])
-    }
-    
-    const obtainedSize = manager.getIntervals().length;
-  
-    // Assert
-    expect(obtainedSize).toEqual(VeryLargeNumber);
-  });
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
 
+test("should return [[0,8], [10,11]]", () => {
+  // Arrange
+  const manager = new IntervalManager();
+  const expectedArray = [
+    [0, 8],
+    [10, 11],
+  ];
 
+  // Act
+  const intervals: Interval[] = [
+    [0, 1],
+    [3, 4],
+    [7, 8],
+    [10, 11],
+    [0, 7],
+  ];
+  intervals.forEach((x) => manager.addInterval(x));
 
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
 
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
 
+test("should return [[0,50]] adds the intervals two times", () => {
+  // Arrange
+  const expectedArray = [[0, 50]];
+  const manager = new IntervalManager();
 
-  test("should return [[0,40]]", () => {
-    // Arrange
-    const expectedArray = [[0, 40]];
-    const manager = new IntervalManager()
-    
-    // Act
-    const intervals: Interval[] = [[0,100], [40, 100]]
-    manager.addInterval(intervals[0])
-    manager.removeInterval(intervals[1])
+  // Act
+  const intervals: Interval[] = [0, 3, 1, 2, 4].map((x) => [
+    x * 10,
+    (x + 1) * 10,
+  ]);
+  intervals.forEach((x) => manager.addInterval(x));
+  intervals.forEach((x) => manager.addInterval(x));
 
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
 
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
 
-  test("should return [[20,40]]", () => {
-    // Arrange
-    const expectedArray = [[20, 40]];
-    const manager = new IntervalManager()
-    
-    // Act
-    const intervals: Interval[] = [[10,100], [10, 20], [40, 100]]
-    manager.addInterval(intervals[0])
-    manager.removeInterval(intervals[1])
-    manager.removeInterval(intervals[2])
+test("should return an array with the correct size (VeryLargeNumber)", () => {
+  // Arrange
+  const VeryLargeNumber = 500;
+  const manager = new IntervalManager();
 
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  // Act
+  for (let i = 1; i <= VeryLargeNumber; i++) {
+    manager.addInterval([i, i + 0.5]);
+  }
 
-  test("should return [[20,40]]", () => {
-    // Arrange
-    const expectedArray = [[20, 40]];
-    const manager = new IntervalManager()
-    
-    // Act
-    const intervals: Interval[] = [[10,100], [0, 20], [40, 300]]
-    manager.addInterval(intervals[0])
-    manager.removeInterval(intervals[1])
-    manager.removeInterval(intervals[2])
+  const obtainedSize = manager.getIntervals().length;
 
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  // Assert
+  expect(obtainedSize).toEqual(VeryLargeNumber);
+});
 
-  test("should return []", () => {
-    // Arrange
-    const expectedArray: Interval[] = [];
-    const manager = new IntervalManager()
-    
-    // Act
-    const intervals: Interval[] = [[10,100], [0, 200], [0, 300]]
-    manager.addInterval(intervals[0])
-    manager.removeInterval(intervals[1])
-    manager.removeInterval(intervals[2])
+test("should return [[0,40]]", () => {
+  // Arrange
+  const expectedArray = [[0, 40]];
+  const manager = new IntervalManager();
 
-    const obtainedArray = manager.getIntervals();
-    console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
-  
-    // Assert
-    expect(obtainedArray).toStrictEqual(expectedArray);
-  });
+  // Act
+  const intervals: Interval[] = [
+    [0, 100],
+    [40, 100],
+  ];
+  manager.addInterval(intervals[0]);
+  manager.removeInterval(intervals[1]);
+
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
+
+test("should return [[20,40]]", () => {
+  // Arrange
+  const expectedArray = [[20, 40]];
+  const manager = new IntervalManager();
+
+  // Act
+  const intervals: Interval[] = [
+    [10, 100],
+    [10, 20],
+    [40, 100],
+  ];
+  manager.addInterval(intervals[0]);
+  manager.removeInterval(intervals[1]);
+  manager.removeInterval(intervals[2]);
+
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
+
+test("should return [[20,40]]", () => {
+  // Arrange
+  const expectedArray = [[20, 40]];
+  const manager = new IntervalManager();
+
+  // Act
+  const intervals: Interval[] = [
+    [10, 100],
+    [0, 20],
+    [40, 300],
+  ];
+  manager.addInterval(intervals[0]);
+  manager.removeInterval(intervals[1]);
+  manager.removeInterval(intervals[2]);
+
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
+
+test("should return []", () => {
+  // Arrange
+  const expectedArray: Interval[] = [];
+  const manager = new IntervalManager();
+
+  // Act
+  const intervals: Interval[] = [
+    [10, 100],
+    [0, 200],
+    [0, 300],
+  ];
+  manager.addInterval(intervals[0]);
+  manager.removeInterval(intervals[1]);
+  manager.removeInterval(intervals[2]);
+
+  const obtainedArray = manager.getIntervals();
+  console.log(`obtainedArray: ${JSON.stringify(obtainedArray)}`);
+
+  // Assert
+  expect(obtainedArray).toStrictEqual(expectedArray);
+});
